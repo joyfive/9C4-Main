2dc1070b032e7c5a6dd12b10b1eeb6a69cfee002
joyfive <joy_five@kakao.com>
2022-11-04 15:37:36 +0900

    new branch

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,10 +1,10 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit"
 
 const store = configureStore({
-    reducer: {
-    },
-    //배포 모드일때 리덕스 데브툴 사용 안함
-    devTools: process.env.REACT_APP_MOD !== 'production'
-});
+  reducer: {},
+  //배포 모드일때 리덕스 데브툴 사용 안함
+  devTools: process.env.REACT_APP_MOD !== "production",
+})
 
-export default store;
+export default store
+//
